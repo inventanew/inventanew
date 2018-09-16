c4d7e6d7c88a536e2eb49ce237e2ec33803877db
John Blakley <inventanew@gmail.com>
2018-09-15 23:55:03 -0400

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,29 +13,9 @@ const Index = ({ data }) => {
       <SEO />
       <Container>
         <PageTitle small>
-          <a
-            href="https://www.gatsbyjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Gatsby
-          </a>,{' '}
-          <a
-            href="https://www.contentful.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contentful
-          </a>{' '}
-          and{' '}
-          <a
-            href="https://www.netlify.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify
-          </a>{' '}
-          <span>🎉</span>
+          <p>
+            Engaging Projects that Inspire Innovation
+          </p>
         </PageTitle>
         <CardList>
           {posts.map(({ node: post }) => (
